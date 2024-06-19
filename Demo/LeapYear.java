d8c4f8bfe07661e8db554846656e40f438d639b4
ravindrasomvanshi1922 <ravindrasomvanshi507@gmail.com>
2024-06-19 18:54:59 +0530

    Core Java Programs

---
+++ Demo/LeapYear.java
@@ -0,0 +1,21 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		
+		 int Year = 0;
+		 System.out.println("Enter Year: ");
+		 Scanner sc = new Scanner(System.in);
+		 
+		 if(Year%4==0) {
+			 System.out.println("This is Leap Year");
+		 }
+		 
+		 
+
+	}
+
+}
